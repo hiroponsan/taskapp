d4db9abf95227f10392175936cffec8e6fb7d5da
hiroponsan <Chuo-univTanaka@outlook.jp>
2016-02-29 21:57:41 +0900

    Initial Commit

---
+++ InputViewController.swift
@@ -0,0 +1,35 @@
+//
+//  InputViewController.swift
+//  taskapp
+//
+//  Created by 田中　博成 on 2016/02/29.
+//  Copyright © 2016年 hiropon. All rights reserved.
+//
+
+import UIKit
+
+class InputViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
