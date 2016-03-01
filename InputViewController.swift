8ccd187456932318b6620f355a400a8d7f5e82ea
hiroponsan <Chuo-univTanaka@outlook.jp>
2016-03-01 16:22:46 +0900

    copied and pasted

--- InputViewController.swift
+++ InputViewController.swift
@@ -7,11 +7,28 @@
 //
 
 import UIKit
-
+import RealmSwift
 class InputViewController: UIViewController {
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var contentsTextView: UITextView!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var category: UITextField!
+    
+    let realm = try! Realm()
+    
+    var task:Task!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tapGesture: UITapGestureRecognizer = UITapGestureRecognizer(target:self, action:"dismissKeyboard")
+        self.view.addGestureRecognizer(tapGesture)
+        
+        titleTextField.text = task.title
+        contentsTextView.text = task.contents
+        datePicker.date = task.date
+        category.text = task.category
 
         // Do any additional setup after loading the view.
     }
@@ -21,6 +38,20 @@ class InputViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    override func viewWillDisappear(animated: Bool) {
+        try! realm.write {
+            self.task.title = self.titleTextField.text!
+            self.task.contents = self.contentsTextView.text
+            self.task.date = self.datePicker.date
+            self.realm.add(self.task, update: true)
+        }
+        
+        super.viewWillDisappear(animated)
+    }
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
 
     /*
     // MARK: - Navigation
--- taskapp/ViewController.swift
+++ taskapp/ViewController.swift
@@ -13,16 +13,22 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // Do any additional setup after loading the view, typically from a nib.
     }
     
+    func dismissKeyboard(){
+        // キーボードを閉じる
+        view.endEditing(true)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
     
+    
     // MARK: UITableViewDataSourceプロトコルのメソッド
     // データの数（＝セルの数）を返すメソッド
     func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-         return taskArray.count
+        return taskArray.count  // ←追加する
     }
     
     // 各セルの内容を返すメソッド
@@ -30,15 +36,25 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
         // 再利用可能な cell を得る
         let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
         
+        // Cellに値を設定する // ←以降、実際のデータを表示するように修正/追加する
+        let task = taskArray[indexPath.row]
+        cell.textLabel?.text = task.title
+        
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm"
+        
+        let dateString:String = formatter.stringFromDate(task.date)
+        cell.detailTextLabel?.text = dateString
+        
         return cell
-    }
-    
-    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        performSegueWithIdentifier("cellSegue",sender: nil) // ←追加する
     }
     
     // MARK: UITableViewDelegateプロトコルのメソッド
     // 各セルを選択した時に実行されるメソッド
+    func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("cellSegue",sender: nil)
+    }
+    
     // セルが削除が可能なことを伝えるメソッド
     func tableView(tableView: UITableView, editingStyleForRowAtIndexPath indexPath: NSIndexPath)-> UITableViewCellEditingStyle {
         return UITableViewCellEditingStyle.Delete
@@ -46,8 +62,37 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     
     // Delete ボタンが押された時に呼ばれるメソッド
     func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
+        if editingStyle == UITableViewCellEditingStyle.Delete {
+            // データベースから削除する  // ←以降追加する
+            try! realm.write {
+                self.realm.delete(self.taskArray[indexPath.row])
+                tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Fade)
+            }
+        }
+    }
+
+    // segue で画面遷移するに呼ばれる
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?){
+        let inputViewController:InputViewController = segue.destinationViewController as! InputViewController
+        
+        if segue.identifier == "cellSegue" {
+            let indexPath = self.tableView.indexPathForSelectedRow
+            inputViewController.task = taskArray[indexPath!.row]
+        } else {
+            let task = Task()
+            task.date = NSDate()
+            
+            if taskArray.count != 0 {
+                task.id = taskArray.max("id")! + 1
+            }
+            
+            inputViewController.task = task
+        }
+    }
+    // 入力画面から戻ってきた時に TableView を更新させる
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        tableView.reloadData()
     }
     
-    
 }
-
